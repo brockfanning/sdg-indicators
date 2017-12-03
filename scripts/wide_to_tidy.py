daacfa33d56cce741eb2f860f505c5ef5b4ac97c
Brock Fanning <brockfanning@gmail.com>
2017-12-03 01:21:18 -0500

    Working wide_to_tidy script

--- scripts/wide_to_tidy.py
+++ scripts/wide_to_tidy.py
@@ -6,84 +6,108 @@ and convert any that are suitable into tidy (long) form.
 Suitability is determined by the presence of column names that follow
 a strict naming convention. The rules of this convention are:
 
-1. The first column is called "year".
-2. The second column is called "total".
-3. At least one column following the format [category]:[value].
-4. Multiple category/value pairs can be separated by a pipe (|).
+1. There is a column is called "Year".
+2. There is a column is called "Total".
+3. Optionally, there are columns following the format: category:value
+4. Optionally, there are columns following the format: category1:value1|category2|value2
 
+Here are some examples of valid headers:
+* Year, Total
+* Year, Total, Gender:Female, Gender:Male
+* Year, Total, Age:Under 18, Age:18 to 64
+* Year, Total, Gender:Female|Age:Under 18, Gender:Male|Age:Under 18
+etc...
 """
 
 import glob
 import os.path
 import pandas as pd
+import numpy as np
 
-def check_headers(df, csv):
+# Some variables to be treated as constants in functions below.
+HEADER_YEAR = 'Year'
+HEADER_TOTAL = 'Total'
+HEADER_VALUE = 'Value'
+
+def tidy_headers_check(df):
     """This checks to see if the column headers are suitable for tidying."""
-    status = True
-    cols = df.columns
 
-    if cols[0] != 'year':
-        status = False
-        print(csv, ': First column not called "year"')
-    if cols[1] != 'total':
-        status = False
-        print(csv, ': Second column not called "total"')
+    columns = df.columns.tolist()
 
-    return status
-
-def wide_to_tidy(df, csv):
-
-    categories = dict()
-    columns = list(df.columns.values)
-    # Look for all the columns following the naming convention for
-    # disaggregation: "[Category]:[Value]" (eg, "Gender:Female")
-    for column in columns:
-        categories_in_column = column.split('|')
-        for category_in_column in categories_in_column:
-            category_parts = category_in_column.split(':')
-            if len(category_parts) > 1:
-                category_name = category_parts[0]
-                category_value = category_parts[1]
-                if category_name not in categories:
-                    categories[category_name] = []
-                categories[category_name].append(category_value)
-                # Rename the column now because we no longer need the naming
-                # convention.
-                df.rename(columns = {column: category_value}, inplace = True)
-    # If we didn't find anything, no tidying is possible.
-    if not categories:
-        print('No categories found.')
+    if HEADER_YEAR not in columns:
         return False
 
-    # "Melt" each category and concat them for a tidy format.
-    melts = []
-    for category in categories:
-        melts.append(pd.melt(df, id_vars=['year'], value_vars=categories[category], var_name=category))
-    # Also add the totals.
-    total = pd.melt(df, id_vars=['year'], value_vars=['total'], var_name='total')
-    del total['total']
-    melts.append(total)
-    tidy = pd.concat(melts)
+    if HEADER_TOTAL not in columns:
+        return False
 
-    # For human readability, move the 'year' column to the front.
+    return True
+
+def tidy_blank_dataframe():
+    """This starts a blank dataframe with our required tidy columns."""
+
+    blank = pd.DataFrame({HEADER_YEAR:[], HEADER_VALUE:[]})
+    blank[HEADER_YEAR] = blank[HEADER_YEAR].astype(int)
+
+    return blank
+
+def tidy_melt(df, value_var, var_name):
+    """This runs a Pandas melt() call with common parameters."""
+
+    return pd.melt(
+        df,
+        id_vars=[HEADER_YEAR],
+        value_vars=[value_var],
+        var_name=var_name,
+        value_name=HEADER_VALUE)
+
+def tidy_dataframe(df):
+    """This converts the data from wide to tidy, based on the column names."""
+
+    tidy = tidy_blank_dataframe()
+    for column in df.columns.tolist():
+        if column == HEADER_TOTAL:
+            # The 'Total' column gets converted into rows without any categories.
+            melted = tidy_melt(df, HEADER_TOTAL, HEADER_TOTAL)
+            del melted[HEADER_TOTAL]
+            tidy = tidy.append(melted)
+        elif '|' not in column and ':' in column:
+            # Columns matching the pattern 'category:value' get converted into
+            # rows where 'category' is set to 'value'.
+            category_parts = column.split(':')
+            category_name = category_parts[0]
+            category_value = category_parts[1]
+            melted = tidy_melt(df, column, category_name)
+            melted[category_name] = category_value
+            tidy = tidy.append(melted)
+        elif '|' in column and ':' in column:
+            # Columns matching the pattern 'category1:value1|category2:value2'
+            # get converted to rows where 'category1' is set to 'value1' and
+            # 'category2' is set to 'value2'.
+            merged = tidy_blank_dataframe()
+            categories_in_column = column.split('|')
+            for category_in_column in categories_in_column:
+                category_parts = category_in_column.split(':')
+                category_name = category_parts[0]
+                category_value = category_parts[1]
+                melted = tidy_melt(df, column, category_name)
+                melted[category_name] = category_value
+                merged = merged.merge(melted, on=[HEADER_YEAR,HEADER_VALUE], how='outer')
+            tidy = tidy.append(merged)
+
+    # For human readability, move 'Year' to the front, and 'Value' to the back.
     cols = tidy.columns.tolist()
-    cols.insert(0, cols.pop(-1))
+    cols.pop(cols.index(HEADER_YEAR))
+    cols.pop(cols.index(HEADER_VALUE))
+    cols = [HEADER_YEAR] + cols + [HEADER_VALUE]
     tidy = tidy[cols]
+
+    # Clean up rows with empty values.
+    tidy.dropna(subset=[HEADER_VALUE], inplace=True)
 
     return tidy
 
 def tidy_csv(csv):
-    """Convert wide CSV file to tidy format
-
-    If there are any problems return False to stop the build.
-
-    Args:
-        csv (str): The file name that we want to tidy
-
-    Returns:
-        bool: Status
-    """
-    status = True
+    """This runs all checks and processing on a CSV file and reports exceptions."""
 
     try:
         df = pd.read_csv(csv)
@@ -91,42 +115,46 @@ def tidy_csv(csv):
         print(csv, e)
         return False
 
-    if not check_headers(df, csv):
+    if not tidy_headers_check(df):
+        # For now, don't output error, since most will fail this check.
+        #print('CSV file ' + csv + ' did not have valid column names.')
         return False
 
     try:
-        tidy = wide_to_tidy(df, csv)
+        tidy = tidy_dataframe(df)
     except Exception as e:
         print(csv, e)
         return False
 
     try:
         csv_file = os.path.split(csv)[-1]
-        tidy_file = csv_file.replace('indicator', 'tidy.indicator')
-        tidy_path = os.path.join('data', 'tidy', tidy_file)
+        tidy_path = os.path.join('data', 'tidy', csv_file)
         tidy.to_csv(tidy_path, index=False, encoding='utf-8')
+        print('Converted ' + csv_file + ' to tidy format.')
     except Exception as e:
         print(csv, e)
         return False
 
-    return status
+    return True
 
 def main():
-    """Tidy up all of the indicator CSVs in the data folder"""
+    """Tidy up all of the indicator CSVs in the data folder."""
+
     status = True
     # Create the place to put the files
     os.makedirs("data/tidy", exist_ok=True)
 
-    csvs = glob.glob("data/convert-to-tidy/indicator*.csv")
-    print("Tidying " + str(len(csvs)) + " wide CSV files...")
+    csvs = glob.glob("data/indicator*.csv")
+    print("Attempting to tidy " + str(len(csvs)) + " wide CSV files...")
 
     for csv in csvs:
         status = status & tidy_csv(csv)
-    return(status)
+
+    return status
 
 if __name__ == '__main__':
     status = main()
-    if (not status):
+    if not status:
         raise RuntimeError("Failed tidy conversion")
     else:
-        print ("Success")
+        print("Success")
